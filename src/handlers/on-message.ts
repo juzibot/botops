2e2d6e53e84ea6aea07ad717db6ab0eab8e322a7
qhduan <mail@qhduan.com>
2019-12-24 00:34:41 +0800

    feat: add default mention

--- src/handlers/on-message.ts
+++ src/handlers/on-message.ts
@@ -9,4 +9,14 @@ export default async function onMessage (
   message : Message,
 ): Promise<void> {
   log.info('on-message', 'onMessage(%s)', message)
+  const room = message.room()
+  // const contact = message.from()
+  const mentions = await message.mention()
+  if (room && mentions) {
+    for (const mention of mentions) {
+      if (mention.id === message.wechaty.self().id) {
+        await room.say('我什么都不会呢at我也没用')
+      }
+    }
+  }
 }
